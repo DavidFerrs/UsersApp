cdad034a53549c6d9c78c59b969aa79d27b126e5
DavidFerrs <fernandes.-.david@hotmail.com>
2025-03-07 22:23:45 -0300

    Initial Commit

---
+++ UsersList/UsersListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersListApp.swift
+//  UsersList
+//
+//  Created by David Ferreira on 07/03/25.
+//
+
+import SwiftUI
+
+@main
+struct UsersListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
